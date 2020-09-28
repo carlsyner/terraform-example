770e8910d382223566efe527cfee52d370f70f3a
Carl Syner <35304728+carlsyner@users.noreply.github.com>
2020-09-28 15:00:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,13 +12,29 @@ provider "azurerm" {
   features {}
 }
 
-## Resource group                                
+## Resource group                                   
 
-resource "azurerm_resource_group" "terraform-terrified-rg" {
-  name     = "terraform-terrified-rg"
-  location = "UK South"
-
-  tags = {
-    environment = "demo"
-  }
+resource "azurerm_resource_group" "terraform-demo-rg" {
+  name     = "Terraform-Demo-RG"
+  location = "West Europe"
 }
+
+                              
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+    keepers = {
+        resource_group = azurerm_resource_group.terraform-demo-rg.name
+    }    
+    byte_length = 3
+}
+
+## Create Storage Account using Local and Unique ID                               
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                      = "terraform${random_id.randomId.hex}"
+  resource_group_name       = azurerm_resource_group.terraform-demo-rg.name
+  location                  = "West Europe"
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
+  account_kind              = "StorageV2"
+ }
