05cda654e2a070696b7b4a197f427213f5f9ac25
Carl Syner <35304728+carlsyner@users.noreply.github.com>
2020-10-05 13:13:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,6 +35,6 @@ resource "azurerm_storage_account" "storageaccount" {
   resource_group_name       = azurerm_resource_group.terraform-demo-rg.name
   location                  = "West Europe"
   account_tier              = "Standard"
-  account_replication_type  = "GRS"
+  account_replication_type  = "LRS"
   account_kind              = "StorageV2"
  }
